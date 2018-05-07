d2733b908411e8d2531b143f5a25ec575f89ff7a
Matthew DEVILLE <mdeville@e1r4p5.42.fr>
2018-05-07 18:16:57 +0200

    Add atoi_base

--- libft/includes/conversion.h
+++ libft/includes/conversion.h
@@ -6,7 +6,7 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/17 15:24:32 by mdeville          #+#    #+#             */
-/*   Updated: 2018/01/18 12:18:07 by mdeville         ###   ########.fr       */
+/*   Updated: 2018/05/07 18:16:26 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@
 char				*ft_utoa_base(uintmax_t n, const char *base);
 char				*ft_utoa(unsigned int n);
 int					ft_atoi(const char *str);
+int					ft_atoi_base(
+							const char *str,
+							const char *base,
+							const char *whitespace,
+							char minus);
 unsigned int		ft_atou(const char *str);
 intmax_t			ft_atoimax(const char *str);
 char				*ft_itoa(int n);
