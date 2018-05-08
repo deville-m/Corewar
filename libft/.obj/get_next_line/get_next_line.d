4e43e82a615f7cdc311449ae445295a3edac6a39
Clement Trosa <me@trosa.io>
2018-05-08 10:03:03 +0200

    stfu travis

---
+++ libft/.obj/get_next_line/get_next_line.d
@@ -0,0 +1,9 @@
+get_next_line/get_next_line.o: srcs/get_next_line/get_next_line.c \
+  includes/ft_string.h includes/get_next_line.h \
+  includes/../../include/op.h
+
+includes/ft_string.h:
+
+includes/get_next_line.h:
+
+includes/../../include/op.h:
