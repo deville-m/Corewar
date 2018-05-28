2e8215ee7c45f9dde380b9051c266e9e30be0596
Robin BARAUD <rbaraud@student.42.fr>
2018-05-28 19:17:10 +0200

    Corrects sub

--- src/corewar_src/instructions/add_sub.c
+++ src/corewar_src/instructions/add_sub.c
@@ -6,7 +6,7 @@
 /*   By: rbaraud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/22 16:05:06 by rbaraud           #+#    #+#             */
-/*   Updated: 2018/05/28 19:00:43 by rbaraud          ###   ########.fr       */
+/*   Updated: 2018/05/28 19:16:51 by rbaraud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,8 @@ void	sub(t_arena *map, t_process *proc)
 	unsigned int	b;
 
 	(void)map->cycle_to_die;
-	a = proc->param[0].data.reg_nbr;
-	b = proc->param[1].data.reg_nbr;
+	a = proc->reg[proc->param[0].data.reg_nbr];
+	b = proc->reg[proc->param[1].data.reg_nbr];
 	a = a - b;
 	proc->reg[proc->param[2].data.reg_nbr] = a;
 	update_carry(proc, a);
