224705aad5aa9e819b4e27712e7f8b9a0babc346
Clement Trosa <me@trosa.io>
2018-05-30 18:16:09 +0200

    updated test

--- corewar_test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-i=0
-diff <(./corewar -d $i -n -1 "$@") <(./resources/corewar_sample -d $i "$@")
-while [ $? -ne 0 ]
-do
-	i=$((i+1))
-	diff <(./corewar -d $i "$@") <(./resources/corewar_sample -d $i "$@")
-done
