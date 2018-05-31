f0ede866b3563b612edc299646c982da20d92718
Clement Trosa <me@trosa.io>
2018-05-31 12:13:28 +0200

    To be honest, I do not quite remember everything I changed here today. But it is all good, I tell ya.

--- src/corewar_src/instructions/aff.c
+++ src/corewar_src/instructions/aff.c
@@ -6,7 +6,7 @@
 /*   By: rbaraud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 14:15:48 by rbaraud           #+#    #+#             */
-/*   Updated: 2018/05/28 14:24:04 by rbaraud          ###   ########.fr       */
+/*   Updated: 2018/05/31 11:54:53 by ctrouill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ void	aff(t_arena *map, t_process *proc)
 	(void)map->cycle_to_die;
 	tmp = proc->reg[proc->param[0].data.reg_nbr];
 	c = (unsigned char)(tmp % 256);
-	write(1, (void *)&c, 1);
+	if (map->opts->verbose == 1)
+		write(1, (void *)&c, 1);
 }
