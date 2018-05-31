64b83cfb45b569266e3a0515fe7f84a09a002daf
Clement Trosa <me@trosa.io>
2018-05-31 18:28:09 +0200

    I should get a raise for this.

--- champion/dwarf.s
+++ champion/dwarf.s
@@ -7,7 +7,7 @@
 	.comment "╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳"
 
 _stub:  sti r1,%:b0i,%1         ; .eh_frame_hdr
-		sti r11, %:l1v3, %1
+        sti r11, %:l1v3, %1
         ld %0, r16
         ld %10, r8
         xor r2,r2,r2
@@ -16,13 +16,14 @@ w00t:	zjmp %:p0p              ; .init ──────┐
 p0p:    zjmp %:t3k0             ; .fortress_start
 
 t3k0:	fork %:l1v3
-		fork %:t0rp
-		zjmp %:b0i
+        fork %:t0rp
+        zjmp %:b0i
 
-t0rp:							; Todo launch torpille
+t0rp:   sti r4, r5, r7
+        zjmp %:t0rp
 
 l1v3:   live %1337
-		zjmp %:l1v3
+        zjmp %:l1v3
 
 b0i:    fork %:l1v3
         zjmp %:w00t             ; .init ──────╯
