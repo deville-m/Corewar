05be14fb1e5aa9709191b624da9378a598e67273
Matthew DEVILLE <mdeville@student.42.fr>
2018-06-01 13:41:24 +0200

    debloat dwark.s

--- champion/dwarf.s
+++ champion/dwarf.s
@@ -9,11 +9,10 @@
 _stub:   st     r1,6            ; .eh_frame_hdr
          live   %42
          ld     %480,r10		; Const offset
-         ld     %478,r14
          fork   %:0x02
          fork   %:0x04
 
-0x01:    st     r1,95           ; .init ──────┐
+0x01:    st     r1,90           ; .init ──────┐
          ldi    %:l1v3,%-3,r5   ; .fortress_start
          and    r16,r16,r16
          zjmp   %:l1v3
@@ -32,7 +31,6 @@ _stub:   st     r1,6            ; .eh_frame_hdr
 0x04:    ld     %12,r11
          ldi    %:l1v3,%9,r5
          and    r16,r16,r16
-         live   %42
          zjmp   %:l1v3
 
 l1v3:    live  %42
